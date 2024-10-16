79eb662bccd0a74442cf7b6b64a2306c36684967
muhonja <linahmuhonjaimbari@gmail.com>
2024-10-16 11:37:35 +0300

    add SweetAlert2 and its css

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import '@/index.css'
 import '@/assets/material-icons/material-icons.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueSweetalert2)
 
 app.mount('#app')
